d80d3aa9adc2cc72785fc8860afc97a60a019eba
Javier <javier.martinezrom@gmail.com>
2024-11-25 11:22:35 +0000

    Completed task 2 GET/api, test passing, app and controller.js added, README updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,20 @@
+// contains static data
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+// allows endpoint connections, supertest provides its own listeners when running tests
+const request = require("supertest");
+const app = require("../app");
+// sample data provided for testing and seed to be populated with such data
+const testData = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+// database connection pool for ending connection after tests run
+const db = require("../db/connection");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(testData);
+});
+afterAll(() => {
+  return db.end();
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
