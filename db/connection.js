a4ca3b570ff18961b389b7712c543216ed64b001
Javier <javier.martinezrom@gmail.com>
2024-11-28 11:15:07 +0000

    Supabase and Render hosting

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,8 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.eng.DATABASE_URL) {
-  throw new Error("PGDATABASE OR DATABASE_URL not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config = {};
